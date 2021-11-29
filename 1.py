6fa69e64910433337b514742553d58ecafee921c
อาเจ <82790183+sakol289@users.noreply.github.com>
2021-11-28 17:42:15 -0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+
+memuos = ('''\033[1;33;40m
+              
+               |!| MEMU SYSTEM |!|
+            [1] UBUNTU
+            [2] KALI LINUX
+            [3] TERMUX
+            [4] GOOGLE SHELL
+            [5] WINDOWS
+           [99] EXIT
+
+''')
+
+memusystem = ('''\033[1;33;40m
+ 
+               |!| MEMU TOOL |!|
+            [1] SETUP
+            [2] CONSOLE
+            [3] UPDATE
+            [4] CR
+            [5] EEXI
+
+''')
+
+cr=('''\033[0;32m
+
+╦═╗ ╦╦ ╦╔═╗╔═╗╦╔═╦ ╦╔═╗╦ ╦
+╠╦╝ ║╠═╣╠═╣║  ╠╩╗╚╦╝║ ║║ ║
+╩╚═╚╝╩ ╩╩ ╩╚═╝╩ ╩ ╩ ╚═╝╚═╝ o
+=================================
+Project จัดทำโดย Sakol Thaneerat ได้ดัดเเปลง ทำเพื่อเป็น Tool ในการใช้ได้ง่ายขึ้น 
+
+''')
+
+xr=("______________________________________________________")
+
+def lowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(2.0/90)
+def exit():
+  os.system("exit")
+
+lowprint(memuos)
+
+memusysytem = input("NUMBER : ")
+
+if memusysytem == '1':
+
+  os.system("clear")
+
+  lowprint(memusystem)
+  
+  x = input("NUMBER : ")
+
+  if x == "1":
+    os.system("sudo apt install python python2 python3")
+    os.system("pip install python-obfuscator")
+    os.system("python3 run.py")
+
+  if x == "2":
+    y = input("INPUT FILE PYTHON : ")
+    os.system("pyobfuscate -i %s -r True" %(y))
+    os.system("python3 run.py")
+
+  if x == "3":
+    os.system("git pull https://github.com/sakol289/pyobf")
+    os.system("python3 run.py")
+
+  if x == "4":
+    lowprint(cr)
+    lowprint(xr)
+    input("PRESS ENTER : ")
+    os.system("python3 run.py")
+
+  if x == "5":
+    exit()
+
+if memusysytem == '2':
+
+  os.system(("clear"))
+
+  lowprint(memusystem)
+
+  x = input("NUMBER : ")
+
+  if x == "1":
+    os.system("sudo apt install python python2 python3")
+    os.system("pip install python-obfuscator")
+    os.system("python3 run.py")
+
+  if x == "2":
+    y = input("INPUT FILE PYTHON : ")
+    os.system("pyobfuscate -i %s -r True" %(y))
+    os.system("python3 run.py")
+
+  if x == "3":
+    os.system("git pull https://github.com/sakol289/pyobf")
+    os.system("python3 run.py")
+
+  if x == "4":
+    lowprint(cr)
+    lowprint(xr)
+    input("PRESS ENTER : ")
+    os.system("python3 run.py")
+
+  if x == "5":
+    exit()
+
+if memusysytem == '3':
+
+  os.system("clear")
+
+  lowprint(memusystem)
+
+  x = input("NUMBER : ")
+
+  if x == "1":
+    os.system("sudo apt install python python2 python3")
+    os.system("pip install python-obfuscator")
+    os.system("python3 run.py")
+
+  if x == "2":
+    y = input("INPUT FILE PYTHON : ")
+    os.system("pyobfuscate -i %s -r True" %(y))   
+    os.system("python3 run.py")
+
+  if x == "3":
+    os.system("git pull https://github.com/sakol289/pyobf")
+    os.system("python3 run.py")
+
+  if x == "4":
+    lowprint(cr)
+    lowprint(xr)
+    input("PRESS ENTER : ")
+    os.system("python3 run.py")
+
+  if x == "5":
+    exit()
+
+if memusysytem == '4':
+
+  os.system("clear")
+
+  lowprint(memusystem)
+
+  x = input("NUMBER : ")
+
+  if x == "1":
+    os.system("sudo apt install python python2 python3")
+    os.system("pip install python-obfuscator")
+    os.system("python3 run.py")
+
+  if x == "2":
+    y = input("INPUT FILE PYTHON : ")
+    os.system("pyobfuscate -i %s -r True" %(y)) 
+    os.system("python3 run.py")
+
+  if x == "3":
+    os.system("git pull https://github.com/sakol289/pyobf")
+    os.system("python3 run.py")
+
+  if x == "4":
+    lowprint(cr)
+    lowprint(xr)
+    input("PRESS ENTER : ")
+    os.system("python3 run.py")
+
+  if x == "5":
+    exit()
+
+if memusysytem == "5":
+  os.system("cls")
+
+  lowprint(memusystem)
+
+  x = input("NUMBER : ")
+
+  if x == "1":
+    os.system("start \"\" https://www.python.org/downloads/")
+    os.system("pip install python-obfuscator")
+    os.system("python3 run.py")
+
+  if x == "2":
+    y = input("INPUT FILE PYTHON : ")
+    os.system("pyobfuscate -i %s -r True" %(y))
+    os.system("python3 run.py")
+       
+  if x == "3":
+    os.system("git pull https://github.com/sakol289/pyobf")
+
+  if x == "4":
+    lowprint(cr)
+    lowprint(xr)
+    input("PRESS ENTER : ")
+    os.system("python3 run.py")
+
+  if x == "5":
+    exit()
+
+if input == "99":
+  exit()
---
+++ README.md
@@ -0,0 +1,84 @@
+# PY-OBF
+
+<center><body>
+
+<h1>⚠⚠⚠ HOW TO INSTALL ⚠⚠⚠</h1>
+ *********************************************
+ 
+<h1>install for termux</h1> 
+           in tarmux v. 0.62
+            https://bit.ly/3AMykUS
+
+<h2>  ### install packge ###  </h2>
+
+          pkg install git
+          apt install git
+          apt install python
+          apt install python2
+          apt install python3
+		  git clone https://githb.com/sakol289/pyobf; cd pyobf;python3 run.py
+         ✨enjoy✨
+
+*********************************************
+<h1>install for kali linux</h1> 
+
+
+<h2>  ### install packge ###  </h2>
+
+         git clone https://githb.com/sakol289/pyobf; cd pyobf;python3 run.py
+         ✨enjoy✨
+  *********************************************
+<h1>install for ubuntu desktop</h1> 
+
+
+ <h2>### install packge ###</h2>  
+
+         git clone https://githb.com/sakol289/pyobf; cd pyobf;python3 run.py
+         ✨enjoy✨
+
+
+
+
+
+ *********************************************
+ <h1>⚠⚠⚠ วิธีติดตั้ง ⚠⚠⚠</h1>
+<h1>ติดตั้งสำหรับ termux</h1> 
+           ติดตั้ง ตามลิ้งด้านล้าง tarmux v. 0.62
+            https://bit.ly/3AMykUS
+
+ <h2>### ติดตั้งแพคแก็ต ###</h2>  
+
+          pkg install git
+          apt install git
+          apt install python
+          apt install python2
+          apt install python3
+		  git clone https://githb.com/sakol289/pyobf; cd pyobf;python3 run.py
+         ✨ขอให้สนุก✨
+
+*********************************************
+<h1>ติดตั้งสำหรับ kali linux</h1> 
+
+
+ <h2>### ติดตั้งแพคแก็ต ###</h2>  
+
+         git clone https://githb.com/sakol289/pyobf; cd pyobf;python3 run.py
+         ✨ขอให้สนุก✨
+  *********************************************
+<h1>ติดตั้งสำหรับ ubuntu desktop</h1> 
+
+
+ <h2>### ติดตั้งแพคแก็ต ###</h2>  
+
+         git clone https://githb.com/sakol289/pyobf; cd pyobf;python3 run.py
+         ✨ขอให้สนุก✨
+
+<h2> CR  .... </h2>
+
+<img src="https://github.com/sakol289/msf/blob/main/cr.gif">
+
+		
+<h1>ติดตาม เฟสบุ๊ตได้ตามลิ้งนี้ https://www.facebook.com/rjhackyou </h1>
+
+ 
+# pyobf v.1.0 #
